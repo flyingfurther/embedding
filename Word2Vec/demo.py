4415699e664619ee529f6715d2212da1800b2de4
flyingfurther <jithonorzhao@gmail.com>
2018-04-17 20:10:16 +0800

    new file:   GloVe-1.2/LICENSE 	new file:   GloVe-1.2/Makefile 	new file:   GloVe-1.2/README 	new file:   GloVe-1.2/corpus.txt 	new file:   GloVe-1.2/demo.sh 	new file:   GloVe-1.2/eval/matlab/WordLookup.m 	new file:   GloVe-1.2/eval/matlab/evaluate_vectors.m 	new file:   GloVe-1.2/eval/matlab/read_and_evaluate.m 	new file:   GloVe-1.2/eval/octave/WordLookup_octave.m 	new file:   GloVe-1.2/eval/octave/evaluate_vectors_octave.m 	new file:   GloVe-1.2/eval/octave/read_and_evaluate_octave.m 	new file:   GloVe-1.2/eval/python/evaluate.py 	new file:   GloVe-1.2/eval/question-data/._capital-common-countries.txt 	new file:   GloVe-1.2/eval/question-data/._capital-world.txt 	new file:   GloVe-1.2/eval/question-data/._city-in-state.txt 	new file:   GloVe-1.2/eval/question-data/._currency.txt 	new file:   GloVe-1.2/eval/question-data/._family.txt 	new file:   GloVe-1.2/eval/question-data/._gram1-adjective-to-adverb.txt 	new file:   GloVe-1.2/eval/question-data/._gram2-opposite.txt 	new file:   GloVe-1.2/eval/question-data/._gram3-comparative.txt 	new file:   GloVe-1.2/eval/question-data/._gram4-superlative.txt 	new file:   GloVe-1.2/eval/question-data/._gram5-present-participle.txt 	new file:   GloVe-1.2/eval/question-data/._gram6-nationality-adjective.txt 	new file:   GloVe-1.2/eval/question-data/._gram7-past-tense.txt 	new file:   GloVe-1.2/eval/question-data/._gram8-plural.txt 	new file:   GloVe-1.2/eval/question-data/._gram9-plural-verbs.txt 	new file:   GloVe-1.2/eval/question-data/capital-common-countries.txt 	new file:   GloVe-1.2/eval/question-data/capital-world.txt 	new file:   GloVe-1.2/eval/question-data/city-in-state.txt 	new file:   GloVe-1.2/eval/question-data/currency.txt 	new file:   GloVe-1.2/eval/question-data/family.txt 	new file:   GloVe-1.2/eval/question-data/gram1-adjective-to-adverb.txt 	new file:   GloVe-1.2/eval/question-data/gram2-opposite.txt 	new file:   GloVe-1.2/eval/question-data/gram3-comparative.txt 	new file:   GloVe-1.2/eval/question-data/gram4-superlative.txt 	new file:   GloVe-1.2/eval/question-data/gram5-present-participle.txt 	new file:   GloVe-1.2/eval/question-data/gram6-nationality-adjective.txt 	new file:   GloVe-1.2/eval/question-data/gram7-past-tense.txt 	new file:   GloVe-1.2/eval/question-data/gram8-plural.txt 	new file:   GloVe-1.2/eval/question-data/gram9-plural-verbs.txt 	new file:   GloVe-1.2/src/cooccur.c 	new file:   GloVe-1.2/src/glove.c 	new file:   GloVe-1.2/src/shuffle.c 	new file:   GloVe-1.2/src/vocab_count.c 	new file:   Word2Vec/README 	new file:   Word2Vec/corpus.txt 	new file:   Word2Vec/demo.py

---
+++ Word2Vec/demo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from gensim.models import word2vec
+from gensim.models.keyedvectors import KeyedVectors
+sentences = word2vec.Text8Corpus("corpus.txt")
+model = word2vec.Word2Vec(sentences, size=50)
+model.wv.save_word2vec_format("embedding.bin", binary=True)
+model = KeyedVectors.load_word2vec_format("embedding.bin", binary=True)
+model.save_word2vec_format("embedding.txt", binary=False)
