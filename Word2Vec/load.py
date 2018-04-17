39ae1903a576f60fb52eb52b2b4550179e5b0694
flyingfurther <jithonorzhao@gmail.com>
2018-04-17 22:52:15 +0800

    Update load.py

--- Word2Vec/load.py
+++ Word2Vec/load.py
@@ -2,7 +2,7 @@
 from gensim.models import word2vec
 from gensim.models.keyedvectors import KeyedVectors
 sentences = word2vec.Text8Corpus("corpus.txt")
-model = word2vec.Word2Vec(sentences, size=50, min_count=5)
+model = word2vec.Word2Vec(sentences, size=50, min_count=2)
 model.wv.save_word2vec_format("embedding.bin", binary=True)
 model = KeyedVectors.load_word2vec_format("embedding.bin", binary=True)
 model.save_word2vec_format("embedding.txt", binary=False)
